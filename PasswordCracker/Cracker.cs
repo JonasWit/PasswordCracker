8907223e79199d29d4dd407347cf57228a66cb31
ViersJW <jonaszwitek@gmail.com>
2021-10-30 09:39:07 +0200

    Add project files.

---
+++ PasswordCracker/Cracker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordCracker
+{
+    public struct CharSet
+    {
+        public const string Characters = @"ąęźżćłśĄĘĆŚŹŻŁabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+}{?>.<*-,./';][\|`~";
+    }
+
+    public class Cracker : IEnumerable
+    {
+        private readonly string _password;
+        private readonly string _charSet;
+        private StringBuilder sb = new StringBuilder();
+
+        private ulong _lenght;
+        private uint _minLenght;
+        private uint _maxLenght;
+
+        public Cracker(string password, string charSet)
+        {
+            _password = password;
+            _charSet = charSet;
+            _maxLenght = 16;
+            _minLenght = 1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            _lenght = (ulong)_charSet.Length;
+            for (var x = _minLenght; x <= _maxLenght; x++)
+            {
+                ulong total = (ulong)Math.Pow((double)_charSet.Length, (double)x);
+                ulong counter = 0;
+                while (counter < total)
+                {
+                    string a = Factor(counter, x - 1);
+                    yield return a;
+                    counter++;
+                }
+            }
+        }
+        private string Factor(ulong counter, double power)
+        {
+            sb.Length = 0;
+            while (power >= 0)
+            {
+                sb = sb.Append(_charSet[(int)(counter % _lenght)]);
+                counter /= _lenght;
+                power--;
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ PasswordCracker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PasswordCracker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Input password or leave empty to quit: ");
+                var password = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(password)) break;
+
+                var cracker = new Cracker(password, CharSet.Characters);
+                ulong passwordsChecked = 0;
+
+                Parallel.ForEach(cracker, p => 
+                {
+                    Console.Write($"Passwords checked: {passwordsChecked++} - {p}\r");
+                    if (p == password)
+                    {
+                        Console.WriteLine("target found:" + p);
+                        return;
+                    }
+                });
+
+                //foreach (string result in cracker)
+                //{
+                //    Console.Write($"Passwords checked: {passwordsChecked++} - {result}\r");
+                //    if (result == password)
+                //    {
+                //        Console.WriteLine("target found:" + result);
+                //        return;
+                //    }
+                //}
+
+                Console.WriteLine("Test Finished!");
+            }
+        }
+    }
+}
